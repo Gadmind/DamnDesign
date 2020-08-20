b31f9762dadcbb31e3669bd98ffd6b6b3d78d285
Gadmind <daopudu@sina.com>
2020-08-20 17:03:17 +0800

    枚举单例模式（Enum）

---
+++ src/main/java/com/daop/design/creational/singleton/EnumSingleton.java
@@ -0,0 +1,16 @@
+package com.daop.design.creational.singleton;
+
+/**
+ * @BelongsProject: damndesign
+ * @BelongsPackage: com.daop.design.creational.singleton
+ * @Description: 枚举单例模式
+ * @DATE: 2020-08-20
+ * @AUTHOR: Administrator
+ **/
+public enum EnumSingleton {
+
+    INSTANCE;
+    public void EnumGetInstance(){
+
+    }
+}
